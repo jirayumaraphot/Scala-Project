c2bf5a2df1486d01447183a34ac2cf785a2b01f5
jirayumaraphot <67070023@kmitl.ac.th>
2026-02-18 21:17:47 +0700

    add file reader and test printing

--- movie/build.sbt
+++ movie/build.sbt
@@ -3,10 +3,13 @@ val scala3Version = "3.8.1"
 lazy val root = project
   .in(file("."))
   .settings(
-    name := "movie",
+    name := "test",
     version := "0.1.0-SNAPSHOT",
 
     scalaVersion := scala3Version,
 
-    libraryDependencies += "org.scalameta" %% "munit" % "1.0.0" % Test
+    libraryDependencies ++= Seq(
+      "org.scalameta" %% "munit" % "1.0.0" % Test,
+      "com.opencsv" % "opencsv" % "5.9"
+    )
   )
--- movie/src/main/scala/Main.scala
+++ movie/src/main/scala/Main.scala
@@ -1,5 +1,41 @@
-@main def hello(): Unit =
-  println("Hello world!")
-  println(msg)
+import com.opencsv.CSVReader
+import java.io.FileReader
+import scala.jdk.CollectionConverters.*
 
-def msg = "I was compiled by Scala 3. :)"
+case class Movie(
+  id: Int,
+  title: String,
+  overview: String,
+  release_date: String,
+  popularity: Double,
+  vote_average: Double,
+  vote_count: Int
+)
+
+@main def run(): Unit =
+  // สามารถเปลี่ยนที่ตั้งของ file ได้
+  val reader = CSVReader(new FileReader("C:/Users/User/Documents/GitHub/Scala-Project/top_rated_movies.csv"))
+
+  val rows = reader.readAll().asScala.toList
+  reader.close()
+
+  val movies =
+    rows.tail.map { row =>
+      Movie(
+        row(0).toInt,
+        row(1),
+        row(2),
+        row(3),
+        row(4).toDouble,
+        row(5).toDouble,
+        row(6).toInt
+      )
+    }
+
+  println(s"Loaded ${movies.length} movies")
+
+  println("\nTop 10 Highest Rated:")
+  movies
+    .sortBy(- _.vote_average)
+    .take(10)
+    .foreach(m => println(s"${m.title} - ${m.vote_average}"))
